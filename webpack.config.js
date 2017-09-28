a2afde6f5888b1e331dd55c8d04cd00d56d10867
Jonathan Grim <jonjongrim@gmail.com>
2017-09-28 16:18:42 -0400

    Add CSS loading and initial CSS reset

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,10 @@ const config = {
         test: /\.(js)$/,
         exclude: /node_modules/,
         use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
